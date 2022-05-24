ab3b2fb98b80d1c0493891bf056bc39e04d9251a
aanhntm <106065029+aanhntm@users.noreply.github.com>
2022-05-24 18:08:06 +0700

    Create main.go

---
+++ Rest API test/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type shoppingList struct {
+	UID       string  `json:"uid"`
+	Item      string  `json:"item"`
+	Total     float64 `json:"total"`
+	Successed bool    `json:"successed"`
+}
+
+var orders = []shoppingList{
+	{UID: "1", Item: "Banana", Total: 500, Successed: false},
+	{UID: "2", Item: "Egg", Total: 1000, Successed: true},
+	{UID: "3", Item: "Pomelo", Total: 700, Successed: false},
+}
+
+func getOders(context *gin.Context) {
+	context.IndentedJSON(http.StatusOK, orders)
+}
+
+func postOrders(context *gin.Context) {
+	var newOrder shoppingList
+
+	if err := context.BindJSON(&newOrder); err != nil {
+		return
+	}
+
+	orders = append(orders, newOrder)
+
+	context.IndentedJSON(http.StatusCreated, newOrder)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/todos", getOders)
+	router.POST("/todos", postOrders)
+	router.Run("localhost:9090")
+}
